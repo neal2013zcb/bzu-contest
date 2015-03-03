600a69352ca4f920e221421bfbe30215b3524214
Bo Zhuang <sdzhuangbo@126.com>
2015-03-03 23:20:49 +0800

    测试 Person 的 phone,qq,weixin 正则表达式约束

---
+++ bzu-contest/test/unit/bzu/contest/PersonTests.groovy
@@ -0,0 +1,97 @@
+package bzu.contest
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Person)
+class PersonTests {
+
+    void "test right phone numbers"() {
+	   mockForConstraintsTests Person
+	   def person = new Person()
+	   def right = [
+		   '',
+		   '12388888888',
+		   '7777777',
+		   '8888888',
+		   '123-88888888',
+		   '1234-88888888',
+		   '8888888-1',
+		   '7777777-22',
+		   '123-88888888-333',
+		   '1234-88888888-4444']
+	   
+	   right.each {
+		   person.phone = it
+		   assert person.validate(['phone'])
+	   }	   
+    }
+
+	void "test wrong phone numbers"() {
+	   mockForConstraintsTests Person
+	   def person = new Person()
+	   def wrong = ['1237777777','123999999999',
+		   '666666','55555','4444','333','22','1',
+		   '44-88888888','55555-88888888']
+	   
+	   wrong.each {
+		   person.phone = it
+		   assert !person.validate(['phone'])
+		   assert 'matches' == person.errors['phone']
+	   }
+	}
+	
+	void "test right qq number"() {
+		mockForConstraintsTests Person
+		def person = new Person()
+		def right = ['','666666','7777777','88888888','99999999','1010101010']
+		
+		right.each { qq->
+			person.qq = qq
+			assert person.validate(['qq'])
+		}
+	}
+	
+	void "test wrong qq number"() {
+	   mockForConstraintsTests Person
+	   def person = new Person()
+	   def wrong = ['1','22','333','4444','55555','99999999911',
+		   '066666','0777777','08888888','09999999','0010101010']
+	   
+	   wrong.each { qq->
+		   person.qq = qq
+		   assert !person.validate(['qq'])
+		   assert 'matches' == person.errors['qq']
+	   }
+	}
+	
+	void "test right weixin number"() {
+		mockForConstraintsTests Person
+		def person = new Person()
+		def right = ['','666666','aaaaaa','aB-123_45',
+			'11111111112222222222']
+		
+		right.each { weixin->
+			person.weixin = weixin
+			assert person.validate(['weixin'])
+		}
+
+	}
+	
+	void "test wrong weixin number"() {
+		mockForConstraintsTests Person
+		def person = new Person()
+		def wrong = ['1','22','333','4444','55555','aaaa@b']
+		
+		wrong.each { weixin->
+			person.weixin = weixin
+			assert !person.validate(['weixin'])
+			assert 'matches' == person.errors['weixin']
+		}
+	}
+}
