9f1ecd9c80701fd58ef2194847f0876435ac23fb
Bo Zhuang <sdzhuangbo@126.com>
2015-03-04 01:43:43 +0800

    创建领域类 bzu.ClassGrade 班级

---
+++ bzu-contest/grails-app/domain/bzu/ClassGrade.groovy
@@ -0,0 +1,35 @@
+package bzu
+
+/**
+ * 班级
+ * 
+ * @author zhbo
+ *
+ */
+class ClassGrade {
+	/**
+	 * 班级名称（简称，唯一的）
+	 */
+	String name
+	/**
+	 * 年级
+	 */
+	Integer grade
+	/**
+	 * 班号
+	 */
+	Integer classNo
+	/**
+	 * 专业
+	 */
+	Specialty specialty
+	
+	static belongsTo = [specialty:Specialty]
+
+    static constraints = {
+		name nullable:false, blank:false, unique:true, maxSize:50
+		grade nullable:false, range:2000..2020
+		classNo nullable:false, range:1..20
+		specialty nullable:false
+    }
+}
