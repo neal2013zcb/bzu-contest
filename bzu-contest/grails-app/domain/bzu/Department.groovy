d0aef24f0d01f321f0f1280b0ae5705e5c90cca6
Bo Zhuang <sdzhuangbo@126.com>
2015-03-04 17:11:57 +0800

    增加类别（1系院，2部门，0其他）

--- bzu-contest/grails-app/domain/bzu/Department.groovy
+++ bzu-contest/grails-app/domain/bzu/Department.groovy
@@ -1,24 +1,28 @@
 package bzu
 
 /**
- * 部门
+ * 单位
  * 
  * @author zhbo
  *
  */
 class Department {
 	/**
-	 * 部门编号（唯一的）
+	 * 单位编号（唯一的）
 	 */
 	String no
 	/**
-	 * 部门名称（全称）
+	 * 单位名称（全称）
 	 */
 	String name
 	/**
-	 * 部门简称
+	 * 单位简称
 	 */
 	String shortName
+	/**
+	 * 类别（1院系，2部门，0其他）
+	 */
+	String category
 	
 	/**
 	 * 各专业
@@ -35,6 +39,7 @@ class Department {
 		no nullable:false, blank:false, unique:true, size:3..20
 		name nullable:false, blank:false, maxSize:50
 		shortName nullable:false, blank:false, maxSize:50
+		category nullable:false, blank:false, inList:['1','2','0'], maxSize:1
     }
 	
 	static mapping = {
