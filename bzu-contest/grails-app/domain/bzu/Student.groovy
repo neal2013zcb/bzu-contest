868262db6de759ce545300d5b1673b265ce7114c
Bo Zhuang <sdzhuangbo@126.com>
2015-03-07 04:37:39 +0800

    增加访问学号、姓名、专业、系院的方法

--- bzu-contest/grails-app/domain/bzu/Student.groovy
+++ bzu-contest/grails-app/domain/bzu/Student.groovy
@@ -15,6 +15,34 @@ class Student {
 	 * 所在班级
 	 */
 	ClassGrade classGrade
+	/**
+	 * 学号
+	 * @return
+	 */
+	String getNo() {
+		person.no
+	}
+	/**
+	 * 学生姓名
+	 * @return
+	 */
+	String getName() {
+		person.name
+	}
+	/**
+	 * 所学专业
+	 * @return
+	 */
+	Specialty getSpecialty() {
+		classGrade.specialty
+	}
+	/**
+	 * 所在系院
+	 * @return
+	 */
+	Department getDepartment() {
+		classGrade.specialty.department
+	}
 	
 	static belongsTo = [classGrade:ClassGrade]
 
