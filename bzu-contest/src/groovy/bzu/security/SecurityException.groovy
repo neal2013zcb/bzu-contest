b2883df63fe17a5ddefd3fe77e9b4d792a6f817e
Bo Zhuang <sdzhuangbo@126.com>
2015-03-15 11:00:40 +0800

    create SecurityException

---
+++ bzu-contest/src/groovy/bzu/security/SecurityException.groovy
@@ -0,0 +1,14 @@
+package bzu.security
+
+/**
+ * 与安全相关的异常
+ * 
+ * @author zhbo
+ *
+ */
+class SecurityException extends RuntimeException {
+	
+	SecurityException(String msg) {
+		super(msg)
+	}
+}
