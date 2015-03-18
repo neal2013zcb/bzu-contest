5f41505d027effd9d6782ddb184ca58e99fb4bbb
Bo Zhuang <sdzhuangbo@126.com>
2015-03-19 00:21:15 +0800

    注册功能测试（空）

---
+++ bzu-contest/test/unit/bzu/security/RegisterControllerTests.groovy
@@ -0,0 +1,20 @@
+package bzu.security
+
+
+
+import grails.test.mixin.*
+
+import org.junit.*
+
+import bzu.security.RegisterController;
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(RegisterController)
+class RegisterControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
