569ceb66e31c12f927518de89f3aae8633fd0745
Bo Zhuang <sdzhuangbo@126.com>
2015-03-22 20:25:39 +0800

    重构：继承 Person

--- bzu-contest/grails-app/domain/bzu/Staff.groovy
+++ bzu-contest/grails-app/domain/bzu/Staff.groovy
@@ -6,38 +6,15 @@ package bzu
  * @author zhbo
  *
  */
-class Staff {
-	/**
-	 * 个人信息
-	 */
-	Person person
+class Staff extends Person {
 	/**
 	 * 所在单位
 	 */
 	Department department
-	/**
-	 * 员工编号
-	 * @return
-	 */
-	String getNo() {
-		person.no
-	}
-	/**
-	 * 员工姓名
-	 * @return
-	 */
-	String getName() {
-		person.name
-	}
 	
 	static belongsTo = [department:Department]
 
     static constraints = {
-		person nullable:false, unique:true // 唯一的
 		department nullable:false
     }
-	
-	String toString() {
-		person.toString()
-	}
 }
