5e2edfe64e935d996414053e9af98136955a79f0
Bo Zhuang <sdzhuangbo@126.com>
2015-03-28 18:38:48 +0800

    为专业管理功能增加访问权限，并禁止系院管理员修改其他系院的专业

--- bzu-contest/grails-app/controllers/bzu/SpecialtyController.groovy
+++ bzu-contest/grails-app/controllers/bzu/SpecialtyController.groovy
@@ -1,24 +1,44 @@
 package bzu
 
+import grails.plugins.springsecurity.Secured;
+
 import org.springframework.dao.DataIntegrityViolationException
 
 class SpecialtyController {
 
     static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+	
+	def beforeInterceptor = [action: this.&checkDepartment, except: ['index','list','show','create']]
+	
+	def userService
+	
+	private checkDepartment() {
+		def myDepartmentId = userService.currentDepartment.id
+		def depId = params.long('department.id', 0) ?: Specialty.get(params.long('id'))?.department?.id
+		if(myDepartmentId != depId) {
+			displayFlashMessage text:'对不起，您不能修改其他系院的信息', type:'error'
+			redirect controller:'errors', action:'forbidden'
+			return false
+		}
+	}
 
+	@Secured(['ROLE_USER'])
     def index() {
         redirect(action: "list", params: params)
     }
 
+	@Secured(['ROLE_USER'])
     def list(Integer max) {
         params.max = Math.min(max ?: 10, 100)
         [specialtyInstanceList: Specialty.list(params), specialtyInstanceTotal: Specialty.count()]
     }
 
+	@Secured(['ROLE_DEPARTMENT'])
     def create() {
         [specialtyInstance: new Specialty(params)]
     }
 
+	@Secured(['ROLE_DEPARTMENT'])
     def save() {
         def specialtyInstance = new Specialty(params)
         if (!specialtyInstance.save(flush: true)) {
@@ -30,6 +50,7 @@ class SpecialtyController {
         redirect(action: "show", id: specialtyInstance.id)
     }
 
+	@Secured(['ROLE_USER'])
     def show(Long id) {
         def specialtyInstance = Specialty.get(id)
         if (!specialtyInstance) {
@@ -41,6 +62,7 @@ class SpecialtyController {
         [specialtyInstance: specialtyInstance]
     }
 
+	@Secured(['ROLE_DEPARTMENT'])
     def edit(Long id) {
         def specialtyInstance = Specialty.get(id)
         if (!specialtyInstance) {
@@ -52,6 +74,7 @@ class SpecialtyController {
         [specialtyInstance: specialtyInstance]
     }
 
+	@Secured(['ROLE_DEPARTMENT'])
     def update(Long id, Long version) {
         def specialtyInstance = Specialty.get(id)
         if (!specialtyInstance) {
@@ -81,6 +104,7 @@ class SpecialtyController {
         redirect(action: "show", id: specialtyInstance.id)
     }
 
+	@Secured(['ROLE_DEPARTMENT'])
     def delete(Long id) {
         def specialtyInstance = Specialty.get(id)
         if (!specialtyInstance) {
