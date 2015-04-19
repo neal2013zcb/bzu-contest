03be85f250ce487bc696dcbb6813d35c5a799daa
Bo Zhuang <sdzhuangbo@126.com>
2015-04-19 22:48:09 +0800

    赛队 Team 的关联

--- bzu-contest/grails-app/domain/bzu/contest/Team.groovy
+++ bzu-contest/grails-app/domain/bzu/contest/Team.groovy
@@ -1,6 +1,21 @@
 package bzu.contest
 
+import bzu.Staff;
+import bzu.Student;
+
 class Team {
+	Project project
+	String name
+	Student pincipal
+	List<Staff> advisors
+	List<Student> members
+	Prize prize
+	
+	static hasOne = [prize : Prize]
+	
+	static belongsTo = [project : Project]
+	
+	static hasMany = [advisors : Staff, members : Student]
 
     static constraints = {
     }
