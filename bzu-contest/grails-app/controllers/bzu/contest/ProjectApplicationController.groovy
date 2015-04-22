b41daa1be91a2829ec9dd06046c4e8765b450c42
Bo Zhuang <sdzhuangbo@126.com>
2015-04-23 01:36:21 +0800

    竞赛项目申请 CRUD

--- bzu-contest/grails-app/controllers/bzu/contest/ProjectApplicationController.groovy
+++ bzu-contest/grails-app/controllers/bzu/contest/ProjectApplicationController.groovy
@@ -1,6 +1,102 @@
 package bzu.contest
 
+import org.springframework.dao.DataIntegrityViolationException
+
 class ProjectApplicationController {
 
-    static scaffold = true
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+	
+	def projectApplicationService
+
+    def index() {
+        redirect(action: "list", params: params)
+    }
+
+    def list(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        [projectApplicationInstanceList: ProjectApplication.list(params), projectApplicationInstanceTotal: ProjectApplication.count()]
+    }
+
+    def save() {
+		params.startDate = params.date('startDate', 'yyyy-MM-dd')
+		params.endDate = params.date('endDate', 'yyyy-MM-dd')
+        def projectApplicationInstance = projectApplicationService.create(params)
+        if (!projectApplicationInstance.save(flush: true)) {
+            render(view: "create", model: [projectApplicationInstance: projectApplicationInstance])
+            return
+        }
+
+        flash.message = message(code: 'default.created.message', args: [message(code: 'projectApplication.label', default: 'ProjectApplication'), projectApplicationInstance.id])
+        redirect(action: "show", id: projectApplicationInstance.id)
+    }
+
+    def show(Long id) {
+        def projectApplicationInstance = ProjectApplication.get(id)
+        if (!projectApplicationInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'projectApplication.label', default: 'ProjectApplication'), id])
+            redirect(action: "list")
+            return
+        }
+
+        [projectApplicationInstance: projectApplicationInstance]
+    }
+
+    def edit(Long id) {
+        def projectApplicationInstance = ProjectApplication.get(id)
+        if (!projectApplicationInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'projectApplication.label', default: 'ProjectApplication'), id])
+            redirect(action: "list")
+            return
+        }
+
+        [projectApplicationInstance: projectApplicationInstance]
+    }
+
+    def update(Long id, Long version) {
+        def projectApplicationInstance = ProjectApplication.get(id)
+        if (!projectApplicationInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'projectApplication.label', default: 'ProjectApplication'), id])
+            redirect(action: "list")
+            return
+        }
+
+        if (version != null) {
+            if (projectApplicationInstance.version > version) {
+                projectApplicationInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+                          [message(code: 'projectApplication.label', default: 'ProjectApplication')] as Object[],
+                          "Another user has updated this ProjectApplication while you were editing")
+                render(view: "edit", model: [projectApplicationInstance: projectApplicationInstance])
+                return
+            }
+        }
+
+        projectApplicationInstance.properties = params
+
+        if (!projectApplicationInstance.save(flush: true)) {
+            render(view: "edit", model: [projectApplicationInstance: projectApplicationInstance])
+            return
+        }
+
+        flash.message = message(code: 'default.updated.message', args: [message(code: 'projectApplication.label', default: 'ProjectApplication'), projectApplicationInstance.id])
+        redirect(action: "show", id: projectApplicationInstance.id)
+    }
+
+    def delete(Long id) {
+        def projectApplicationInstance = ProjectApplication.get(id)
+        if (!projectApplicationInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'projectApplication.label', default: 'ProjectApplication'), id])
+            redirect(action: "list")
+            return
+        }
+
+        try {
+            projectApplicationInstance.delete(flush: true)
+            flash.message = message(code: 'default.deleted.message', args: [message(code: 'projectApplication.label', default: 'ProjectApplication'), id])
+            redirect(action: "list")
+        }
+        catch (DataIntegrityViolationException e) {
+            flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'projectApplication.label', default: 'ProjectApplication'), id])
+            redirect(action: "show", id: id)
+        }
+    }
 }
--- bzu-contest/test/unit/bzu/contest/ProjectApplicationControllerTests.groovy
+++ bzu-contest/test/unit/bzu/contest/ProjectApplicationControllerTests.groovy
@@ -2,16 +2,154 @@ package bzu.contest
 
 
 
-import grails.test.mixin.*
 import org.junit.*
+import grails.test.mixin.*
 
-/**
- * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
- */
 @TestFor(ProjectApplicationController)
+@Mock(ProjectApplication)
 class ProjectApplicationControllerTests {
 
-    void testSomething() {
-       fail "Implement me"
+    def populateValidParams(params) {
+        assert params != null
+        // TODO: Populate valid properties like...
+        //params["name"] = 'someValidName'
+    }
+
+    void testIndex() {
+        controller.index()
+        assert "/projectApplication/list" == response.redirectedUrl
+    }
+
+    void testList() {
+
+        def model = controller.list()
+
+        assert model.projectApplicationInstanceList.size() == 0
+        assert model.projectApplicationInstanceTotal == 0
+    }
+
+    void testCreate() {
+        def model = controller.create()
+
+        assert model.projectApplicationInstance != null
+    }
+
+    void testSave() {
+        controller.save()
+
+        assert model.projectApplicationInstance != null
+        assert view == '/projectApplication/create'
+
+        response.reset()
+
+        populateValidParams(params)
+        controller.save()
+
+        assert response.redirectedUrl == '/projectApplication/show/1'
+        assert controller.flash.message != null
+        assert ProjectApplication.count() == 1
+    }
+
+    void testShow() {
+        controller.show()
+
+        assert flash.message != null
+        assert response.redirectedUrl == '/projectApplication/list'
+
+        populateValidParams(params)
+        def projectApplication = new ProjectApplication(params)
+
+        assert projectApplication.save() != null
+
+        params.id = projectApplication.id
+
+        def model = controller.show()
+
+        assert model.projectApplicationInstance == projectApplication
+    }
+
+    void testEdit() {
+        controller.edit()
+
+        assert flash.message != null
+        assert response.redirectedUrl == '/projectApplication/list'
+
+        populateValidParams(params)
+        def projectApplication = new ProjectApplication(params)
+
+        assert projectApplication.save() != null
+
+        params.id = projectApplication.id
+
+        def model = controller.edit()
+
+        assert model.projectApplicationInstance == projectApplication
+    }
+
+    void testUpdate() {
+        controller.update()
+
+        assert flash.message != null
+        assert response.redirectedUrl == '/projectApplication/list'
+
+        response.reset()
+
+        populateValidParams(params)
+        def projectApplication = new ProjectApplication(params)
+
+        assert projectApplication.save() != null
+
+        // test invalid parameters in update
+        params.id = projectApplication.id
+        //TODO: add invalid values to params object
+
+        controller.update()
+
+        assert view == "/projectApplication/edit"
+        assert model.projectApplicationInstance != null
+
+        projectApplication.clearErrors()
+
+        populateValidParams(params)
+        controller.update()
+
+        assert response.redirectedUrl == "/projectApplication/show/$projectApplication.id"
+        assert flash.message != null
+
+        //test outdated version number
+        response.reset()
+        projectApplication.clearErrors()
+
+        populateValidParams(params)
+        params.id = projectApplication.id
+        params.version = -1
+        controller.update()
+
+        assert view == "/projectApplication/edit"
+        assert model.projectApplicationInstance != null
+        assert model.projectApplicationInstance.errors.getFieldError('version')
+        assert flash.message != null
+    }
+
+    void testDelete() {
+        controller.delete()
+        assert flash.message != null
+        assert response.redirectedUrl == '/projectApplication/list'
+
+        response.reset()
+
+        populateValidParams(params)
+        def projectApplication = new ProjectApplication(params)
+
+        assert projectApplication.save() != null
+        assert ProjectApplication.count() == 1
+
+        params.id = projectApplication.id
+
+        controller.delete()
+
+        assert ProjectApplication.count() == 0
+        assert ProjectApplication.get(projectApplication.id) == null
+        assert response.redirectedUrl == '/projectApplication/list'
     }
 }
